8f7b20e664675c6648e0165cc496376a0d27ceed
Escoli1 <99153649+Escoli1@users.noreply.github.com>
2022-02-08 16:01:38 -0400

    ejemplo 1 de  manejo de PHP

---
+++ ejemplo.php
@@ -0,0 +1,68 @@
+<html>
+	<body>
+		<?php
+			echo "hola mundo";
+		?>
+	<h1></h1>
+	</body>
+<html>
+
+<html>
+	<body>
+		<?php
+			$name="pepe";
+			$year=2021;
+			echo $name;
+		?>
+		<tr>
+
+		<?php
+		echo $year;
+		?>
+		<tr>
+		<?php
+		echo $name."<br>".$year;
+		?>
+		<H1>OPERADORES</H1>
+		<?php
+		$a=2;
+		$b=3;
+		$suma=$a+$b;
+		$resta=$a-$b;
+		$multiplicacion=$a*$b;
+		$division=$a/$b;
+		$modulo=$a%$b;
+		
+		echo "Valor del primer operador ".$a;
+		echo "Valor del segundo operador ".$b;
+		echo "resultado de la suma ".$suma;
+		echo "resultado de la resta ".$resta;
+		echo "resultado de la mumltiplicacion ".$multiplicacion;
+		echo "resultado de la division ".$division;
+		echo "resultado de la modulo ".$modulo;
+		
+
+		?>
+		<H1>INCREMENTO Y  DECREMENTO </H1>
+		<?php
+		$a=2;
+		$b=3;
+		$suma=$a+$b;
+		$resta=$a-$b;
+		$multiplicacion=$a*$b;
+		$division=$a/$b;
+		$modulo=$a%$b;
+		
+		echo "Valor del primer operador ".$a;
+		echo "Valor del segundo operador ".$b;
+		echo "resultado de la suma ".$suma;
+		echo "resultado de la resta ".$resta;
+		echo "resultado de la mumltiplicacion ".$multiplicacion;
+		echo "resultado de la division ".$division;
+		echo "resultado de la modulo ".$modulo;
+		
+
+		?>
+	<h1></h1>
+	</body>
+<html>
